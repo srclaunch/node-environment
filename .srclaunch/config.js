e2e7cc2789ae256f77c3e264a00c46d43de845bb
Steven Bennett <steven@budgetbloom.com>
2022-04-24 03:28:36 -0700

    Quick release - patch

---
+++ .srclaunch/config.js
@@ -0,0 +1,29 @@
+import {
+  BuildFormat,
+  BuildPlatform,
+  BuildTarget,
+  BuildTool,
+  ProjectType,
+  TestReporter,
+} from '@srclaunch/types';
+
+export default {
+  name: '@srclaunch/node-environment',
+  description: 'Node.js environment specific utilities for SrcLaunch services',
+  type: ProjectType.Library,
+  build: {
+    formats: [BuildFormat.ESM, BuildFormat.UMD],
+    platform: BuildPlatform.Node,
+    target: BuildTarget.ESNext,
+    tool: BuildTool.Vite,
+  },
+  test: {
+    coverage: {
+      reporters: [TestReporter.Lcov, TestReporter.JSONSummary],
+    },
+    files: {
+      include: ['src/**/*.test.{js,jsx,ts,tsx}'],
+    },
+    verbose: true,
+  },
+};
