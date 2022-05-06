0bc91ab52d628406802db8b21406dcafb06bb987
Steven Bennett <steven@budgetbloom.com>
2022-05-05 22:12:25 -0700

    chore: Clean installation cache

--- .srclaunch/config.ts
+++ .srclaunch/config.ts
@@ -14,7 +14,7 @@ import {
   TestTool,
 } from '@srclaunch/types';
 
-export default {
+const config: Project = {
   name: '@srclaunch/node-environment',
   description: 'Node.js environment specific utilities for SrcLaunch services',
   type: ProjectType.Library,
@@ -30,11 +30,11 @@ export default {
     },
     tool: TestTool.Ava,
   },
-  environment: {
+  environments: {
     development: {
       formatters: [CodeFormatterTool.Prettier],
       linters: [CodeLinterTool.ESLint],
-      staticTypes: [StaticTypingTool.TypeScript],
+      staticTyping: [StaticTypingTool.TypeScript],
     },
   },
   release: {
@@ -53,4 +53,6 @@ export default {
       types: true,
     },
   },
-} as Project;
+};
+
+export default config;
